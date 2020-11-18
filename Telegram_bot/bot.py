b970b374538ea95484fc2ffa2e6951d5c7004dcb
winkxx <68945988+winkxx@users.noreply.github.com>
2020-11-18 18:14:16 +0800

    Update bot.py

--- Telegram_bot/bot.py
+++ Telegram_bot/bot.py
@@ -7,7 +7,7 @@ class telegram_chatbot():
 
     def __init__(self, config):
         self.token = self.read_token_from_config_file(config)
-        self.base = "https://api.telegram.org/bot{}/".format(1466249391:AAGSHTWYNQquHx1FhNv5NViAsA8yPMVjTbE)
+        self.base = "https://api.telegram.org/bot{}/".format(self.token)
 
     def get_updates(self, offset=None):
         url = self.base + "getUpdates?timeout=100"
@@ -17,7 +17,7 @@ class telegram_chatbot():
         return json.loads(r.content)
 
     def send_message(self, msg, chat_id):
-        url = self.base + "sendMessage?chat_id={}&text={}".format(-356183884, msg)
+        url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
         if msg is not None:
             requests.get(url)
 
